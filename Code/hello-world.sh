ee4a6ec0765472fa277ed31ee91a10294622340b
Daniel Andre ROMERO HERNANDEZ <20009173>
2020-02-28 15:31:05 +0100

    commit DRO 4

--- Code/hello-world.sh
+++ Code/hello-world.sh
@@ -2,9 +2,12 @@
 # Usage: Hello World Bash Shell Script Using Variables
 # -------------------------------------------------
  
-
-var="Hello World"
+var="Hello Wor   ld"
 var2="Fanny Huvig la meilleure du monde !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
 echo "$var"
 echo "$var2"
 exit 1
+echo " ALLEZ LE MEXIQUE!"
+echo " TOTO"
+echo " MDR"
